1262c61f1355627b3aa96c2eb93944057f374452
AaronDeluna <aaron_deluna@mail.ru>
2024-11-03 17:28:23 +0300

    initial commit

---
+++ src/main/java/org/javaacademy/nuclear_power_plant/NuclearPowerPlantApplication.java
@@ -0,0 +1,13 @@
+package org.javaacademy.nuclear_power_plant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NuclearPowerPlantApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NuclearPowerPlantApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/javaacademy/nuclear_power_plant/NuclearPowerPlantApplicationTests.java
@@ -0,0 +1,13 @@
+package org.javaacademy.nuclear_power_plant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NuclearPowerPlantApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
